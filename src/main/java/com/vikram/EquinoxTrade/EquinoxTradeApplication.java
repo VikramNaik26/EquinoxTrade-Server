0d14a355bdb132ca88d223443e12e6f55931d142
Vikram Naik <26vikramnaik@gmail.com>
2024-08-30 16:59:34 +0530

    init: initialize the starter project

---
+++ src/main/java/com/vikram/EquinoxTrade/EquinoxTradeApplication.java
@@ -0,0 +1,13 @@
+package com.vikram.EquinoxTrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquinoxTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquinoxTradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vikram/EquinoxTrade/EquinoxTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikram.EquinoxTrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquinoxTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

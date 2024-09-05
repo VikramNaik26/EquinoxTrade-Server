0b0cf61e6bb68c1ff18c4c88b93901c09144743f
Vikram Naik <26vikramnaik@gmail.com>
2024-09-05 12:13:53 +0530

    refactor: remove unused imports

--- src/main/java/com/vikram/EquinoxTrade/model/ForgotPasswordToken.java
+++ src/main/java/com/vikram/EquinoxTrade/model/ForgotPasswordToken.java
@@ -3,8 +3,6 @@ package com.vikram.EquinoxTrade.model;
 import com.vikram.EquinoxTrade.domain.VerificationType;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 
--- src/main/java/com/vikram/EquinoxTrade/service/AuthService.java
+++ src/main/java/com/vikram/EquinoxTrade/service/AuthService.java
@@ -1,11 +1,7 @@
 package com.vikram.EquinoxTrade.service;
 
-import com.vikram.EquinoxTrade.exception.AuthExcetion;
-import com.vikram.EquinoxTrade.model.TwoFactorOTP;
 import com.vikram.EquinoxTrade.model.UserEntity;
-import com.vikram.EquinoxTrade.repository.UserRepository;
 import com.vikram.EquinoxTrade.response.AuthResponse;
-import com.vikram.EquinoxTrade.utils.OtpUtils;
 
 /**
  * AuthService
